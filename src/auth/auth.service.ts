a888505fdb592824ab56f79a533ef0a67d40a3f6
Binto Joseph <bintojoseph543@gmail.com>
2022-08-19 22:44:16 +0530

    test db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,10 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '@prisma/client';
 import { Request } from 'express';
-import { GetUser } from 'src/auth/decorator';
-import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
